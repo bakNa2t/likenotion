fd0c5f6ed1a30514102dcee799380fd739a857b1
bakNa2t <ok2.lock@gmail.com>
2025-01-02 13:32:53 +0300

    Add Heading and Entities to marketing route

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Entities } from "./_components/Entities";
+import { Heading } from "./_components/Heading";
 
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        <h1 className="text-3xl bold">Likenotion - clone of Notion</h1>
-        <Button>Get started</Button>
+        <Heading />
+        <Entities />
       </div>
     </div>
   );
