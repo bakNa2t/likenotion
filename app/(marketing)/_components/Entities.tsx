9ffe1bbf87e089a02a923a0b6a8aad1b1b233f61
bakNa2t <ok2.lock@gmail.com>
2025-01-03 14:54:32 +0300

    Add Image to Entities

--- app/(marketing)/_components/Entities.tsx
+++ app/(marketing)/_components/Entities.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 export const Entities = () => {
-  return <div>Entities</div>;
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+          <Image src="task.svg" fill className="object-contain" alt="task" />
+        </div>
+
+        <div className="relative w-[400px] h-[400px] hidden md:block">
+          <Image src="squad.svg" fill className="object-contain" alt="squad" />
+        </div>
+      </div>
+    </div>
+  );
 };
