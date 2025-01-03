2bd392b1772ac933a47ada57c12e45d62387b548
bakNa2t <ok2.lock@gmail.com>
2025-01-03 15:40:19 +0300

    Add shadcn button

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { Logo } from "./Logo";
 
 export default function Footer() {
   return (
     <div className="flex items-center w-full p-6 bg-background z-50">
       <Logo />
+      <div className="flex items-center gap-x-2 md:ml-auto w-full justify-between md:justify-end">
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms of Service
+        </Button>
+      </div>
     </div>
   );
 }
