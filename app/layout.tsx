b6968668a0b47dcf108cb59ddfe71001849471a6
bakNa2t <ok2.lock@gmail.com>
2025-01-05 11:42:50 +0300

    Add Theme Provider to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -37,11 +40,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="likenotion-theme"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
