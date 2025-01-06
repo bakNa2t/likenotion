e05db4dd625d2cc9e0b904719392fef4d8b58d31
bakNa2t <ok2.lock@gmail.com>
2025-01-06 14:16:04 +0300

    Update Image to toggle Entities svg for dark/light theme

--- app/(marketing)/_components/Entities.tsx
+++ app/(marketing)/_components/Entities.tsx
@@ -6,11 +6,33 @@ export const Entities = () => {
     <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
-          <Image src="task.svg" fill className="object-contain" alt="task" />
+          <Image
+            src="task.svg"
+            fill
+            className="object-contain dark:hidden"
+            alt="task"
+          />
+          <Image
+            src="task-dark.svg"
+            fill
+            className="object-contain hidden dark:block"
+            alt="task"
+          />
         </div>
 
         <div className="relative w-[400px] h-[400px] hidden md:block">
-          <Image src="squad.svg" fill className="object-contain" alt="squad" />
+          <Image
+            src="squad.svg"
+            fill
+            className="object-contain dark:hidden"
+            alt="squad"
+          />
+          <Image
+            src="squad-dark.svg"
+            fill
+            className="object-contain hidden dark:block"
+            alt="squad"
+          />
         </div>
       </div>
     </div>
