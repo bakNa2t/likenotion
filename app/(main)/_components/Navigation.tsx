62bee3d8652a5209123792ca78b08ae759f0ff4d
bakNa2t <ok2.lock@gmail.com>
2025-01-09 11:53:02 +0300

    Add MenuIcon if isCollapsed true

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import { ComponentRef, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
-import { ChevronsLeft } from "lucide-react";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 
@@ -13,7 +13,7 @@ export const Navigation = () => {
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ComponentRef<"aside">>(null);
   const navbarRef = useRef<ComponentRef<"div">>(null);
-  const [isReseting, setIsReseting] = useState(false);
+  const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   return (
@@ -22,13 +22,16 @@ export const Navigation = () => {
         ref={sidebarRef}
         className={cn(
           "relative group/sidebar w-60 h-full flex flex-col bg-secondary overflow-y-auto z-[99999]",
-          isReseting && "transition-all duration-300 ease-in-out",
+          isResetting && "transition-all duration-300 ease-in-out",
           isMobile && "w-0"
         )}
       >
         <div
           role="button"
-          className="absolute top-3 right-2 w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 opacity-0 group-hover/sidebar:opacity-100 transition"
+          className={cn(
+            "absolute top-3 right-2 w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100"
+          )}
         >
           <ChevronsLeft className="w-6 h-6" />
         </div>
@@ -43,6 +46,20 @@ export const Navigation = () => {
 
         <div className="absolute top-0 right-0 opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize h-full w-1 bg-primary/10" />
       </aside>
+
+      <div
+        className={cn(
+          "absolute top-0 left-60 w-[calc(100%-240px)] z-[99999]",
+          isResetting && "transition-all duration-300 ease-in-out",
+          isMobile && "left-0 w-full"
+        )}
+      >
+        <nav className="w-full px-3 py-2 bg-transparent">
+          {isCollapsed && (
+            <MenuIcon role="button" className="w-6 h-6 text-muted-foreground" />
+          )}
+        </nav>
+      </div>
     </>
   );
 };
