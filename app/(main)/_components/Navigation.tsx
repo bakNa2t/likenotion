d60cb745bc58dcada90ec4d77a38451aafff5ccc
bakNa2t <ok2.lock@gmail.com>
2025-01-09 11:20:21 +0300

    Add ref and state for media queries

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { ComponentRef, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
 import { ChevronsLeft } from "lucide-react";
+import { useMediaQuery } from "usehooks-ts";
+import { cn } from "@/lib/utils";
 
 export const Navigation = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ComponentRef<"aside">>(null);
+  const navbarRef = useRef<ComponentRef<"div">>(null);
+  const [isReseting, setIsReseting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
   return (
     <>
-      <aside className="relative group/sidebar w-60 h-full flex flex-col bg-secondary overflow-y-auto z-[99999]">
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "relative group/sidebar w-60 h-full flex flex-col bg-secondary overflow-y-auto z-[99999]",
+          isReseting && "transition-all duration-300 ease-in-out",
+          isMobile && "w-0"
+        )}
+      >
         <div
           role="button"
           className="absolute top-3 right-2 w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 opacity-0 group-hover/sidebar:opacity-100 transition"
