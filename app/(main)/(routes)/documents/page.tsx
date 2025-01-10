6329cffd5856c42ed69b94a071eb2085608019d9
bakNa2t <ok2.lock@gmail.com>
2025-01-10 11:08:35 +0300

    Add blank svg to page for dark/light theme

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import Image from "next/image";
+
 const DocumentsPage = () => {
-  return <div>Protected page</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-full space-y-4">
+      <Image
+        src="/blank.svg"
+        width="300"
+        height="300"
+        alt="blank"
+        className="dark:hidden"
+      />
+      <Image
+        src="/blank-dark.svg"
+        width="300"
+        height="300"
+        alt="blank"
+        className="hidden dark:block"
+      />
+    </div>
+  );
 };
 
 export default DocumentsPage;
