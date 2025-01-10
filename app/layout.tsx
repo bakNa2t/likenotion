c7f8f9cdcc5722fbcb9fa56bb851efac5b3c10f8
bakNa2t <ok2.lock@gmail.com>
2025-01-10 13:59:48 +0300

    Add Toaster from sonner module

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
@@ -53,6 +54,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="likenotion-theme"
           >
+            <Toaster />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
