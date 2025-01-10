7079a0c300f1bd8c0a80d5d03faa64992328c174
bakNa2t <ok2.lock@gmail.com>
2025-01-10 11:17:10 +0300

    Add Button to create note

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
 import Image from "next/image";
 
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
     <div className="flex flex-col justify-center items-center h-full space-y-4">
       <Image
@@ -19,6 +24,14 @@ const DocumentsPage = () => {
         alt="blank"
         className="hidden dark:block"
       />
+
+      <h2 className="text-lg font-semibold">
+        Welcome to {user?.firstName}&apos;s Likenotion
+      </h2>
+      <Button>
+        <PlusCircle className="w-4 h-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 };
