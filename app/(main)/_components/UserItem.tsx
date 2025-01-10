e39673cde6bc71e0f60ff1efef678e2a9d088145
bakNa2t <ok2.lock@gmail.com>
2025-01-10 12:29:23 +0300

    Create UserItem comp

---
+++ app/(main)/_components/UserItem.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useUser } from "@clerk/clerk-react";
+import { ChevronsLeftRight } from "lucide-react";
+
+export const UserItem = () => {
+  const { user } = useUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
+        >
+          <div className="flex items-center gap-x-2 max-w-[150px]">
+            <Avatar className="w-6 h-6">
+              <AvatarImage src={user?.imageUrl} />
+            </Avatar>
+            <span className="text-start font-medium line-clamp-1">
+              {user?.fullName}&apos;s Likenotion
+            </span>
+          </div>
+          <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground w-4 h-4" />
+        </div>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+};
