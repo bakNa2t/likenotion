c56bf1b09dda9441968fde082610f13113aef1a0
bakNa2t <ok2.lock@gmail.com>
2025-01-10 17:11:36 +0300

    Add onCreate fn by using convex's api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useUser } from "@clerk/clerk-react";
-import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
 import Image from "next/image";
+import { PlusCircle } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "Note created successfully!",
+      error: "Failed to create a note",
+    });
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-full space-y-4">
@@ -28,7 +43,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-semibold">
         Welcome to {user?.firstName}&apos;s Likenotion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="w-4 h-4 mr-2" />
         Create a note
       </Button>
