5c19334af7946d91ad5e8c798df25d8f29306bb5
bakNa2t <ok2.lock@gmail.com>
2025-01-12 17:36:23 +0300

    Styled Item and add passed Icon from props

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -7,6 +7,16 @@ interface ItemProps {
   onClick: () => void;
   icon: LucideIcon;
 }
-export const Item = ({ label, onClick, icon }: ItemProps) => {
-  return <div>{label}</div>;
+export const Item = ({ label, onClick, icon: Icon }: ItemProps) => {
+  return (
+    <div
+      role="button"
+      onClick={onClick}
+      style={{ paddingLeft: "12px" }}
+      className="group min-h-[27px] w-full flex items-center py-1 pr-3 text-sm text-muted-foreground font-medium hover:bg-primary/5"
+    >
+      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      <span className="truncate">{label}</span>
+    </div>
+  );
 };
