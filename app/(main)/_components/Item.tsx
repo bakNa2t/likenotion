f8a6732e29e1cb30f4d8125810d841123833bc56
bakNa2t <ok2.lock@gmail.com>
2025-01-13 10:30:01 +0300

    Add Item's style dynamically and ChevronIcon's variant

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { LucideIcon } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
 
 import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -28,13 +29,29 @@ export const Item = ({
   expanded,
   onExpand,
 }: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       role="button"
       onClick={onClick}
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] w-full flex items-center py-1 pr-3 text-sm text-muted-foreground font-medium hover:bg-primary/5"
+      style={{
+        paddingLeft: level ? `${level * 12 + 12}px` : "12px",
+      }}
+      className={cn(
+        "group min-h-[27px] w-full flex items-center py-1 pr-3 text-sm text-muted-foreground font-medium hover:bg-primary/5",
+        active && "bg-primary/5 text-primary"
+      )}
     >
+      {!!id && (
+        <div
+          role="button"
+          className="h-full mr-1 rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          onClick={() => {}}
+        >
+          <ChevronIcon className="shrink-0 w-4 h-4 text-muted-foreground/50" />
+        </div>
+      )}
       <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
       <span className="truncate">{label}</span>
     </div>
