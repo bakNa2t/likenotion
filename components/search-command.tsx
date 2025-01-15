e1a4a1a1816a3c3a09ed89be0b9c93b63cd7e6db
bakNa2t <ok2.lock@gmail.com>
2025-01-15 17:22:58 +0300

    Create SearchCommand comp

---
+++ components/search-command.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
+
+import { api } from "@/convex/_generated/api";
+import { useSearch } from "@/hooks/useSearch";
+import { File } from "lucide-react";
+
+export const SearchCommand = () => {
+  const router = useRouter();
+  const documents = useQuery(api.documents.getSearch);
+  const { user } = useUser();
+  const [isMounted, setIsMounted] = useState(false);
+
+  const toggle = useSearch((store) => store.toggle);
+  const isOpen = useSearch((store) => store.isOpen);
+  const onClose = useSearch((store) => store.onClose);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <CommandDialog open={isOpen} onOpenChange={onClose}>
+      <CommandInput placeholder={`Search ${user?.fullName}'s notes...`} />
+
+      <CommandList>
+        <CommandEmpty>No notes found.</CommandEmpty>
+        <CommandGroup heading="Notes">
+          {documents?.map((document) => (
+            <CommandItem
+              key={document._id}
+              value={`${document._id}-${document.title}`}
+              title={document.title}
+              onSelect={() => {}}
+            >
+              {document.icon ? (
+                <p className="mr-2 text-[18px]">{document.icon}</p>
+              ) : (
+                <File className="mr-2 w-4 h-4" />
+              )}
+              <span>{document.title}</span>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
+};
