c97c9ac1972162aa919c2b297752eaad4508e74e
bakNa2t <ok2.lock@gmail.com>
2025-01-16 15:07:01 +0300

    Create SettingsModal

---
+++ components/modals/SettingsModal.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useSettings } from "@/hooks/useSettings";
+
+import { ModeToggle } from "../theme-mode-toggle";
+import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
+import { Label } from "../ui/label";
+
+export const SettingsModal = () => {
+  const settings = useSettings();
+
+  return (
+    <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
+      <DialogContent>
+        <DialogHeader className="pb-3 border-b">
+          <h2 className="text-lg font-medium">My Settings</h2>
+        </DialogHeader>
+
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-y-1">
+            <Label>Appearance</Label>
+            <span className="text-[0.8rem] text-muted-foreground">
+              Customize Likenotion&apos;s appearance on your device
+            </span>
+          </div>
+
+          <ModeToggle />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
