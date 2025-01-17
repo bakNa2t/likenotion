2bd1147ba26e2aa88b8316b6ff97768b81d12d23
bakNa2t <ok2.lock@gmail.com>
2025-01-17 10:46:04 +0300

    Create Navbar comp

---
+++ app/(main)/_components/Navbar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+interface NavbarProps {
+  isCollapsed: boolean;
+  onResetWidth: () => void;
+}
+
+export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+  return <div>Navbar Document</div>;
+};
