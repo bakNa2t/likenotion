ee4bbbae6b67143a42ff4240a7de6d1f034ea852
bakNa2t <ok2.lock@gmail.com>
2025-01-17 13:52:12 +0300

    Add MenuIcon to display if sidebar collapsed

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import React from "react";
+import { useParams } from "next/navigation";
+import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -8,5 +14,32 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
-  return <div>Navbar Document</div>;
+  const params = useParams();
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  if (document === null) {
+    return null;
+  }
+
+  return (
+    <>
+      <nav className="w-full flex items-center gap-x-4 px-3 py-2 bg-background dark:bg-[#131313]">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="w-6 h-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">Navbar</div>
+      </nav>
+    </>
+  );
 };
