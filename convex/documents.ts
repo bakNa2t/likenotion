c13f23b331001d5f525ade86db4aac6edb64eb8c
bakNa2t <ok2.lock@gmail.com>
2025-01-17 14:16:08 +0300

    Add update documents mutation handler

--- convex/documents.ts
+++ convex/documents.ts
@@ -260,3 +260,41 @@ export const getById = query({
     return document;
   },
 });
+
+export const update = mutation({
+  args: {
+    id: v.id("documents"),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const { id, ...left } = args;
+
+    const existingDocument = await ctx.db.get(id); // org arg "args.id"
+
+    if (!existingDocument) {
+      throw new Error("Document not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      ...left,
+    });
+
+    return document;
+  },
+});
