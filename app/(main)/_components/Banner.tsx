1af33498bffda41b3cb2e8fcccd98f9f15f3c6b9
bakNa2t <ok2.lock@gmail.com>
2025-01-17 17:11:25 +0300

    Add onRemove/Restore handlers to Button and styles

--- app/(main)/_components/Banner.tsx
+++ app/(main)/_components/Banner.tsx
@@ -2,10 +2,13 @@
 
 import { useMutation } from "convex/react";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import { ConfirmModal } from "@/components/modals/ConfirmModal";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { toast } from "sonner";
 
 interface BannerProps {
   documentId: Id<"documents">;
@@ -18,15 +21,15 @@ export const Banner = ({ documentId }: BannerProps) => {
   const restore = useMutation(api.documents.restore);
 
   const onRemove = () => {
-    const promise = remove({ id: documentId }).then(() => {
-      router.push("/documents");
-    });
+    const promise = remove({ id: documentId });
 
     toast.promise(promise, {
       loading: "Deleting note...",
       success: "Note deleted successfully!",
       error: "Failed to delete note",
     });
+
+    router.push("/documents");
   };
 
   const onRestore = () => {
@@ -39,5 +42,28 @@ export const Banner = ({ documentId }: BannerProps) => {
     });
   };
 
-  return <div>Banner</div>;
+  return (
+    <div className="w-full flex items-center justify-center gap-x-2 p-2 text-white text-sm text-center bg-red-400">
+      <p> This note is in the Trash</p>
+
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={onRestore}
+        className="p-1 px-2 h-auto font-normal border-white text-white bg-transparent hover:bg-primary/5 hover:text-white"
+      >
+        Restore note
+      </Button>
+
+      <ConfirmModal onConfirm={onRemove}>
+        <Button
+          variant="outline"
+          size="sm"
+          className="p-1 px-2 h-auto font-normal border-white text-white bg-transparent hover:bg-primary/5 hover:text-white"
+        >
+          Delete permanently
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
 };
