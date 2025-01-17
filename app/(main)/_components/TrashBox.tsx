506008938f413ff6cdbfc522e794b5600e49dba0
bakNa2t <ok2.lock@gmail.com>
2025-01-17 10:45:07 +0300

    Updated hover bg style for dark theme

--- app/(main)/_components/TrashBox.tsx
+++ app/(main)/_components/TrashBox.tsx
@@ -97,7 +97,7 @@ export const TrashBox = () => {
               <div
                 onClick={(e) => onRestore(e, document._id)}
                 role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200"
+                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
               >
                 <Undo className="w-4 h-4 text-muted-foreground" />
               </div>
@@ -105,7 +105,7 @@ export const TrashBox = () => {
               <ConfirmModal onConfirm={() => onRemove(document._id)}>
                 <div
                   role="button"
-                  className="p-2 roundeed-sm hover:bg-neutral-200"
+                  className="p-2 rounded-sm hover:bg-neutral-200 dark:hover:bg-neutral-600"
                 >
                   <Trash className="w-4 h-4 text-muted-foreground" />
                 </div>
