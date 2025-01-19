bea2e26fdbb20b9a1aeda1ab172c2510df07cb9e
bakNa2t <ok2.lock@gmail.com>
2025-01-19 17:56:36 +0300

    Add DropdownMenu to move document to trash

--- app/(main)/_components/Menu.tsx
+++ app/(main)/_components/Menu.tsx
@@ -3,10 +3,20 @@
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { MoreHorizontal, Trash } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 import { Id } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
-import { toast } from "sonner";
 
 interface MenuProps {
   documentId: Id<"documents">;
@@ -30,5 +40,31 @@ export const Menu = ({ documentId }: MenuProps) => {
     router.push("/documents");
   };
 
-  return <div>Menu</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="sm" variant="ghost">
+          <MoreHorizontal className="w-4 h-4" />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className="w-60"
+        align="end"
+        alignOffset={8}
+        forceMount
+      >
+        <DropdownMenuItem onClick={onArchive} role="button">
+          <Trash className="w-4 h-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <div className="text-xs text-muted-foreground p-2">
+          Last edited by: {user?.fullName}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
