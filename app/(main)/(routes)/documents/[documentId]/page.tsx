d6b9607ebfe789d69fa77fa6b99d4442448d7658
bakNa2t <ok2.lock@gmail.com>
2025-01-20 10:43:33 +0300

    Create DocumentIdPage with convex's useQuery

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="w-full h-full pt-30 text-[1rem] font-bold">
+        DocumentIdPage
+      </h1>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
