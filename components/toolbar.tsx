3e4557249724c006a86d3b0de4d144ab150ed77e
bakNa2t <ok2.lock@gmail.com>
2025-01-20 11:52:30 +0300

    Create Toolbar and add IconPicker

---
+++ components/toolbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { IconPicker } from "./icon-picker";
+
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+}
+
+export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  return (
+    <div className="pl-[54px] group relative">
+      {!!initialData.icon && !preview && (
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={() => {}}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+        </div>
+      )}
+    </div>
+  );
+};
