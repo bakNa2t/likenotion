49fc25f19da664993a099e7d810ce6d525f95a8e
bakNa2t <ok2.lock@gmail.com>
2025-01-20 12:41:00 +0300

    Add Button to add emoji icon

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -3,6 +3,8 @@
 import { IconPicker } from "./icon-picker";
 
 import { Doc } from "@/convex/_generated/dataModel";
+import { Button } from "./ui/button";
+import { Smile, X } from "lucide-react";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -19,8 +21,36 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
               {initialData.icon}
             </p>
           </IconPicker>
+
+          <Button
+            size="icon"
+            variant="outline"
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground"
+          >
+            <X className="w-4 h-4" />
+          </Button>
         </div>
       )}
+
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">{initialData.icon}</p>
+      )}
+
+      <div className="flex items-center gap-x-1 py-4 opacity-0 group-hover:opacity-100">
+        {!initialData.icon && !preview && (
+          <IconPicker onChange={() => {}} asChild>
+            <Button
+              size="sm"
+              variant="outline"
+              className="text-xs text-muted-foreground"
+            >
+              <Smile className="w-4 h-4 mr-2" />
+              Add icon
+            </Button>
+          </IconPicker>
+        )}
+      </div>
     </div>
   );
 };
