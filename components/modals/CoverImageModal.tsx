56510b42c6baaeb7b550f77cfb5a67794e4ea189
bakNa2t <ok2.lock@gmail.com>
2025-01-21 14:47:47 +0300

    Add onChange fn with update mutation to upload file

--- components/modals/CoverImageModal.tsx
+++ components/modals/CoverImageModal.tsx
@@ -1,10 +1,41 @@
 "use client";
 
-import { useCoverImage } from "@/hooks/useCoverImage";
+import { useState } from "react";
+import { useParams } from "next/navigation";
+import { useMutation } from "convex/react";
+
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
 
+import { useEdgeStore } from "@/lib/edgestore";
+import { useCoverImage } from "@/hooks/useCoverImage";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
 export const CoverImageModal = () => {
+  const params = useParams();
+  const [file, setFile] = useState<File>();
+  const [isSubmiting, setIsSubmiting] = useState(false);
+
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmiting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+    }
+  };
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
