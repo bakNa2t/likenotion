20aafdc2e03ca6d623368c1fc6b369c83bdb2752
bakNa2t <ok2.lock@gmail.com>
2025-01-22 13:41:18 +0300

    Add change|remove Button

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
 
 interface CoverImageProps {
   url?: string;
@@ -19,6 +21,30 @@ export const CoverImage = ({ url, preview }: CoverImageProps) => {
       )}
     >
       {!!url && <Image src={url} alt="Cover" fill className="object-cover" />}
+
+      {url && !preview && (
+        <div className="absolute bottom-5 right-5 flex items-center gap-x-2 opacity-0 group-hover:opacity-100">
+          <Button
+            size="sm"
+            variant="outline"
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+          >
+            <ImageIcon className="w-4 h-4 mr-1" />
+            Change cover
+          </Button>
+
+          <Button
+            size="sm"
+            variant="outline"
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+          >
+            <X className="w-4 h-4 mr-1" />
+            Remove cover
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
