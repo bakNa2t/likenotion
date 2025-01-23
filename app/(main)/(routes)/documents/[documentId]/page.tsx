a2061353f4d4d4e9640072b88a7041b68e27cf3a
bakNa2t <ok2.lock@gmail.com>
2025-01-23 14:44:30 +0300

    Add skeleton while document is undefined

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -7,6 +7,7 @@ import { CoverImage } from "@/components/cover-image";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface DocumentIdPageProps {
   params: {
@@ -20,7 +21,20 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   });
 
   if (document === undefined) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <CoverImage.Skeleton />
+
+        <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+          <div className="pt-4 pl-8 space-y-4">
+            <Skeleton className="w-[50%] h-14" />
+            <Skeleton className="w-[80%] h-4" />
+            <Skeleton className="w-[40%] h-4" />
+            <Skeleton className="w-[60%] h-4" />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (document === null) {
