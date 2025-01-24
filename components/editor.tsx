02dacb94574de14feaeb135a9de5a404595c3e51
bakNa2t <ok2.lock@gmail.com>
2025-01-24 14:31:43 +0300

    Add BlockNoteView editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { useTheme } from "next-themes";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/core/style.css";
+import "@blocknote/mantine/style.css";
 
 interface EditorProps {
   initialContent?: string;
@@ -9,5 +14,25 @@ interface EditorProps {
 }
 
 export const Editor = ({ initialContent, editable, onChange }: EditorProps) => {
-  return <div>Editor</div>;
+  const { resolvedTheme } = useTheme();
+
+  const editor: BlockNoteEditor = useCreateBlockNote({
+    editable,
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        editable={editable}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
+    </div>
+  );
 };
