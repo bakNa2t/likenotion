a79bee06f466f2990550629c1ac9ff6283c042cf
bakNa2t <ok2.lock@gmail.com>
2025-01-27 11:50:54 +0300

    Update useCreateBlockNote hook and create onSubmit fn

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,22 +1,38 @@
 "use client";
 
+import { useState } from "react";
 import { useTheme } from "next-themes";
-import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { useMutation } from "convex/react";
+import { Block, BlockNoteEditor } from "@blocknote/core";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
+import { toast } from "sonner";
 
-import { useEdgeStore } from "@/lib/edgestore";
 import "@blocknote/core/style.css";
 import "@blocknote/mantine/style.css";
+import "@blocknote/core/fonts/inter.css";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface EditorProps {
   initialContent?: string;
   editable?: boolean;
+  params: {
+    documentId: Id<"documents">;
+  };
   onChange: (value: string) => void;
 }
 
-const Editor = ({ initialContent, editable, onChange }: EditorProps) => {
+const Editor = ({ initialContent, editable, params }: EditorProps) => {
+  const [blocks, setBlocks] = useState<Block[]>([]);
+  const [value, setValue] = useState<string>(
+    initialContent ? initialContent : ""
+  );
+
   const { resolvedTheme } = useTheme();
+
   const { edgestore } = useEdgeStore();
 
   const handleUpload = async (file: File) => {
@@ -28,15 +44,28 @@ const Editor = ({ initialContent, editable, onChange }: EditorProps) => {
   };
 
   const editor: BlockNoteEditor = useCreateBlockNote({
-    editable,
-    initialContent: initialContent
-      ? (JSON.parse(initialContent) as PartialBlock[])
-      : undefined,
-    onEditorContentChange: (editor) => {
-      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
-    },
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
     uploadFile: handleUpload,
   });
+
+  const update = useMutation(api.documents.update);
+
+  const onSubmit = () => {
+    if (value.trim() !== "") {
+      setValue(JSON.stringify(blocks, null, 2));
+    }
+
+    const promise = update({
+      id: params.documentId,
+      content: JSON.stringify(blocks, null, 2),
+    });
+
+    toast.promise(promise, {
+      loading: "Saving note...",
+      success: "Note saved successfully!",
+      error: "Failed to save note",
+    });
+  };
 
   return (
     <div>
@@ -44,6 +73,7 @@ const Editor = ({ initialContent, editable, onChange }: EditorProps) => {
         editable={editable}
         editor={editor}
         theme={resolvedTheme === "dark" ? "dark" : "light"}
+        onChange={() => setBlocks(editor.document)}
       />
     </div>
   );
