f19271c158d016a4ec43941fcdc5ef7c8244f7e2
bakNa2t <ok2.lock@gmail.com>
2025-01-27 15:21:47 +0300

    Add onPublish/Unpublish and onCopy fn

--- app/(main)/_components/Publish.tsx
+++ app/(main)/_components/Publish.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { useMutation } from "convex/react";
+import { toast } from "sonner";
 
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
 import { useOrigin } from "@/hooks/useOrigin";
-import { useState } from "react";
 
 interface PublishProps {
   initialData: Doc<"documents">;
@@ -19,6 +20,46 @@ export const Publish = ({ initialData }: PublishProps) => {
   const update = useMutation(api.documents.update);
 
   const url = `${origin}/preview/${initialData._id}`;
+
+  const onPublish = () => {
+    setIsSubmiting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: true,
+    }).finally(() => setIsSubmiting(false));
+
+    toast.promise(promise, {
+      loading: "Publishing note...",
+      success: "Note published successfully!",
+      error: "Failed to publish note",
+    });
+  };
+
+  const onUnpublish = () => {
+    setIsSubmiting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: false,
+    }).finally(() => setIsSubmiting(false));
+
+    toast.promise(promise, {
+      loading: "Unpublishing note...",
+      success: "Note unpublished successfully!",
+      error: "Failed to unpublish note",
+    });
+  };
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(url);
+
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
 
   return <div>Publish</div>;
 };
