7003921ac876be0bca8e4f08e3a517680c784368
bakNa2t <ok2.lock@gmail.com>
2025-01-30 11:34:50 +0300

    Update styles for media queries

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -5,12 +5,12 @@ import { useConvexAuth } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
 
 import { Logo } from "./Logo";
-
-import { ModeToggle } from "@/components/theme-mode-toggle";
-import { Spinner } from "@/components/spinner";
-import { useScrollTop } from "@/hooks/use-scroll-top";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+
+import { cn } from "@/lib/utils";
+import { ModeToggle } from "@/components/theme-mode-toggle";
+import { useScrollTop } from "@/hooks/use-scroll-top";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -19,14 +19,14 @@ export const Navbar = () => {
   return (
     <nav
       className={cn(
-        "z-50 bg-background dark:bg-[#131313] fixed top-0 flex items-center w-full p-6",
+        "fixed flex items-center z-50 bg-background dark:bg-[#131313] top-0 w-full p-4 md:p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
       <Logo />
 
       <div className="w-full flex items-center justify-between gap-x-2 md:ml-auto md:justify-end">
-        {isLoading && <Spinner />}
+        {isLoading && <Spinner size="default" />}
         {!isAuthenticated && !isLoading && (
           <>
             <SignInButton mode="modal">
