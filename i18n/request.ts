df758bc753e25324cdf4539e580979659c8dce3c
bakNa2t <ok2.lock@gmail.com>
2025-02-03 13:27:03 +0300

    Add cookies value to update locale

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,11 +1,13 @@
 import { getRequestConfig } from "next-intl/server";
-// import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 
 export default getRequestConfig(async () => {
-  const locale = "en";
+  const cookieLocale =
+    (await cookies()).get("NEXT_TRANSLATION_LOCALE")?.value || "en";
+  const locale = cookieLocale;
 
   return {
     locale,
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
