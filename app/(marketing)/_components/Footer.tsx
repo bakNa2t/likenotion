6e2b106a1eb07b011a339c7294c2d65656ea5d9b
bakNa2t <ok2.lock@gmail.com>
2025-02-04 10:59:58 +0300

    Add footer's intl translations

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -1,16 +1,25 @@
+import { useTranslations } from "next-intl";
+
 import { Button } from "@/components/ui/button";
 import { Logo } from "./Logo";
 
 export function Footer() {
+  const t = useTranslations("Footer");
+
   return (
     <div className="flex items-center w-full p-6 bg-background dark:bg-[#131313] z-50">
-      <Logo />
+      <div className="flex flex-col items-start gap-y-2 w-full">
+        <Logo />
+        <p className="text-xs text-muted-foreground">
+          &copy; {new Date().getFullYear()} Likenotion. All rights reserved.
+        </p>
+      </div>
       <div className="flex items-center gap-x-2 md:ml-auto w-full justify-between md:justify-end">
         <Button variant="ghost" size="sm">
-          Privacy Policy
+          {t("privacy")}
         </Button>
         <Button variant="ghost" size="sm">
-          Terms of Service
+          {t("terms")}
         </Button>
       </div>
     </div>
